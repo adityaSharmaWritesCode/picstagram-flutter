326e2e87ddd90a234e6d9a1726e8d044907bc70a
unknown <shaditya1604@gmail.com>
2022-07-17 19:12:10 +0530

    Added firebase dependencies for Web & Android versions of the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:picstagram/responsive/mobile_layout.dart';
 import 'package:picstagram/responsive/responsive.dart';
 import 'package:picstagram/responsive/web_layout.dart';
 
-void main() {
+void main() async {
+  //Ensures that all the widgets have been built before the next line is executed.
+  //Important as without it the firebase initialiser could run into a null check exception
+  WidgetsFlutterBinding.ensureInitialized();
+  //Initialises Firebase app (the web version requires the options variable, but the mobile version requires it to be null)
+  //Hence, the following if/else condition :
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyCZam7L4OLe8pt9Wl6LaVRQX_2F4tzT9P0",
+        appId: "1:760480030603:web:aeced22e0e63360ac1198e",
+        messagingSenderId: "760480030603",
+        projectId: "picstagram-5d0f8",
+        storageBucket: "picstagram-5d0f8.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -20,13 +40,18 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Picstagram',
-      darkTheme: ThemeData.dark().copyWith(
+      theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: const Color.fromRGBO(0, 0, 0, 1),
       ),
-      home: ResponsiveLayout(
+      // darkTheme: ThemeData.dark().copyWith(
+      //   scaffoldBackgroundColor: const Color.fromRGBO(0, 0, 0, 1),
+      // ),
+      home: const ResponsiveLayout(
         webScreenLayout: WebScreenLayout(),
         mobileScreenLayout: MobileScreenLayout(),
       ),
     );
   }
 }
+
+//TODO : Look up how to write firestore database rules
